0804bf3a1ffc1799952d8d2e9606d2088acb4c54
mcrist <mcristernag@gmail.com>
2023-06-27 01:00:25 -0700

    Dirty hack, stupidy compiler error (::QByteArray name() const). Qt 5.15.2; error : must use 'class' tag to refer to type 'QByteArray' in this scope

--- generators/smoke/writeClasses.cpp
+++ generators/smoke/writeClasses.cpp
@@ -116,6 +116,13 @@ QString SmokeClassFiles::generateMethodBody(const QString& indent, const QString
         if (meth.type()->isFunctionPointer() || meth.type()->isArray())
             out << meth.type()->toString("xret") << " = ";
         else if (meth.type() != Type::Void)
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))       
+          // Dirty hack, stupidy compiler error (::QByteArray name() const). Qt 5.15.2
+          // error : must use 'class' tag to refer to type 'QByteArray' in this scope 
+          if (meth.name().contains("name") &&  meth.type()->toString().contains("QByteArray"))
+            out << "class QByteArray" << " xret = ";
+          else        
+#endif
             out << meth.type()->toString() << " xret = ";
 
         if (!(meth.flags() & Method::Static)) {
@@ -175,7 +182,14 @@ QString SmokeClassFiles::generateMethodBody(const QString& indent, const QString
       if (field == "s_enum")
         out << indent << "x[0]." << field << " = static_cast<long>(" << Util::assignmentString(meth.type(), "xret") << ");\n";
       else
-        out << indent << "x[0]." << field << " = " << Util::assignmentString(meth.type(), "xret") << ";\n";
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))                
+        // Dirty hack, stupidy compiler error (::QByteArray name() const). Qt 5.15.2
+        // error : must use 'class' tag to refer to type 'QByteArray' in this scope 
+        if (meth.name().contains("name") &&  meth.type()->toString().contains("QByteArray"))
+          out <<  indent << "x[0]." << field << " = " << "(void*)new class QByteArray(xret);\n";
+        else
+#endif            
+          out << indent << "x[0]." << field << " = " << Util::assignmentString(meth.type(), "xret") << ";\n";
     } else {
         out << indent << "(void)x; // noop (for compiler warning)\n";
     }
